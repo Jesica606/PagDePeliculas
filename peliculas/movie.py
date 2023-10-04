112a941914dde1e3f7e2e61dfb31cc98403d1462
Jesica606 <jesicaaranibar17@gmail.com>
2023-10-04 12:22:25 -0300

    agregue una pagina para actores, lenguajes y categorias

--- peliculas/movie.py
+++ peliculas/movie.py
@@ -6,12 +6,42 @@ from peliculas.db import get_db
 
 bp = Blueprint('movie', __name__)
 
-@bp.route('/')
-def index():
+@bp.route('/peliculas')
+def index_films():
     db = get_db()
     films = db.execute(
-        'SELECT title, description'
+        'SELECT title, description, rating, release_year'
         ' FROM film'
         ' ORDER BY title ASC'
     ).fetchall()
-    return render_template('movie/index.html', films=films)
+    return render_template('movie/peliculas.html', films=films)
+
+@bp.route('/actores')
+def index_actor():
+    db = get_db()
+    actors = db.execute(
+        'SELECT first_name, last_name'
+        ' FROM actor'
+        ' ORDER BY first_name ASC'
+    ).fetchall()
+    return render_template('movie/actores.html', actors = actors)
+
+@bp.route('/categorias')
+def index_category():
+    db = get_db()
+    categories = db.execute(
+        'SELECT name'
+        ' FROM category'
+        ' ORDER BY name ASC'
+    ).fetchall()
+    return render_template('movie/categorias.html', categories = categories)
+
+@bp.route('/lenguajes')
+def index_languege():
+    db = get_db()
+    languages = db.execute(
+        'SELECT name'
+        ' FROM language'
+        ' ORDER BY name ASC'
+    ).fetchall()
+    return render_template('movie/lenguajes.html', langueges = languages)
